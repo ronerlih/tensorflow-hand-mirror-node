1672f68f4a13fe0a2003fc024a838ad4af736055
ron erlih <ronerlih@gmail.com>
2020-09-05 18:00:55 -0400

    drawing pose and savig img

--- utils/getImagePose.mjs
+++ utils/getImagePose.mjs
@@ -2,6 +2,7 @@ import posenet from '@tensorflow-models/posenet';
 import {readFileSync} from "./readFileSync.mjs";
 import * as tf from '@tensorflow/tfjs-node';
 import Canvas from 'canvas';
+import fs from 'fs';
 
 
 async function bufferToImageData (buff) {
@@ -23,20 +24,38 @@ async function getImagePose() {
    // const imageElement = document.getElementById('cat');
    // readfile for testing
    const data = readFileSync('../images/man-working-with-bad-posture-1200x630.jpg')
-      const imageData = await bufferToImageData(data);
+      const canvas = await bufferToImageData(data);
    const imageScaleFactor = 0.50;
    const flipHorizontal = false;
    const outputStride = 16;
    //convert image file to base64-encoded string
    // var htmlImageElement = new Image(630, 630);
    // console.log(htmlImageElement);
-   console.log(imageData)
    // // load the posenet model
    const net = await posenet.load();
 
-   const pose = await net.estimateSinglePose(imageData, imageScaleFactor, flipHorizontal, outputStride);
-   console.log(pose)
+   const pose = await net.estimateSinglePose(canvas, imageScaleFactor, flipHorizontal, outputStride);
+   console.log(pose);
 
+   // draw
+   const ctx = canvas.getContext('2d');
+
+ 
+   pose.keypoints
+      .filter(point => point.score >= 0.5)
+      .forEach( point => {
+         ctx.beginPath(); //Start path
+         ctx.arc(point.position.x, point.position.y, 3, 0, Math.PI * 2, true); // Draw a point using the arc function of the canvas with a point structure.
+         ctx.fill();
+      })
+   const dataURL = canvas.toDataURL()
+   var matches = dataURL.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+   const newBuf = new Buffer(matches[2], 'base64')
+   console.log(newBuf)
+   var buf = Buffer.from(newBuf, 'base64');
+   await fs.writeFileSync('image.jpg', buf);
+
+   
 }
 getImagePose();
 
